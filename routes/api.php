c1b76cea51c72dfd1cf333a484313e5e36250dff
Gerald Dasalla <dasallagerald@gmail.com>
2021-03-22 10:11:15 +0800

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\AuthController;
+
+Route::group([
+    'prefix' => 'auth',
+    'middleware' => 'api'
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
